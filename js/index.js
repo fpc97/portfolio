eca930e4f4ce7d808e8ff00853eb0f571ed0079f
Francisco <fran.perez.c@hotmail.com>
2021-06-23 01:26:40 -0300

    implement smooth scrolling

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// Smooth scrolling
+if (window.scrollTo && document.querySelectorAll) {
+  var anchors = document.querySelectorAll("a[href^='#']");
+
+  for (var i = 0; i < anchors.length; i++) {
+    (function() {
+      var linkedId = anchors[i].getAttribute("href");
+      var linkedElement = document.getElementById(linkedId.substring(1));
+
+      if (linkedElement) {
+        anchors[i].addEventListener("click", function(e) {
+          e.preventDefault();
+    
+          window.scrollTo({
+            top: linkedElement.offsetTop,
+            behavior: "smooth"
+          });
+        });
+      }
+    })();
+  }
+}
