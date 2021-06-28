9d1ce66a7aa814b71821e8d35ab584af4d299b30
Francisco <fran.perez.c@hotmail.com>
2021-06-28 11:32:53 -0300

    Correct the HTML

--- js/index.js
+++ js/index.js
@@ -7,6 +7,8 @@ if (window.scrollTo && document.querySelectorAll) {
       var linkedId = anchors[i].getAttribute("href");
       var linkedElement = document.getElementById(linkedId.substring(1));
 
+      console.log(linkedId, linkedElement)
+
       if (linkedElement) {
         anchors[i].addEventListener("click", function(e) {
           e.preventDefault();
